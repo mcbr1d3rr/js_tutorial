8e1eb87b40ea0a8e8abf2b07326d67dcfcc4e41b
mcbr1d3rr <randy@rmcbride.com>
2018-10-17 14:55:28 -0600

    update using Form, chap-9 complete

--- main.js
+++ main.js
@@ -6,20 +6,20 @@ let Phrase = require("rmcbride-palindrome");
 // description: prompts for a string & displays results
 // input:
 // return:
-function palindromeTester()
+function palindromeTester(event)
 {
-  let string = prompt("Please enter a string for palindrome testing:");
-  let phrase = new Phrase(string);
+  event.preventDefault();
+
+  //let string = prompt("Please enter a string for palindrome testing:");
+  let phrase = new Phrase(event.target.phrase.value);
   let palindromeResult = document.querySelector("#palindromeResult");
 
   if (phrase.palindrome())
   {
-    //alert(`"${phrase.content}" is a palindrome!`);
     palindromeResult.innerHTML = `<strong>"${phrase.content}"</strong> is a palindrome!`;
   }
   else
   {
-    //alert(`"${phrase.content}" is not a palindrome.`)
     palindromeResult.innerHTML = `"${phrase.content}" is not a palindrome.`;
   }
 }
@@ -27,19 +27,18 @@ function palindromeTester()
 // listens for web document to load
 document.addEventListener("DOMContentLoaded", function()
 {
-
+/*
   let button = document.querySelector("#palindromeTester");
   button.addEventListener("click", function()
   {
     palindromeTester();
   });
-
-  // use form instead of button
-/*
-  let form = document.querySelector("#palindromeTester");
-  form.addEventListener("submit", function()
-  {
-    palindromeTester();
-  });
 */
+  // use submit form instead of click button
+
+  let tester = document.querySelector("#palindromeTester");
+  tester.addEventListener("submit", function(event)
+  {
+    palindromeTester(event);
+  });
 });
